aaf9141b9f7a6ea441a6fb4c02f6233c8126173e
aneeshc12 <c.aneesh.1203@gmail.com>
2023-04-06 17:15:29 +0530

    finish DEM generation, begin work on the DYT

--- generateDEM.py
+++ generateDEM.py
@@ -77,84 +77,6 @@ def draw_registration_result(source, target, transformation):
     source_temp.transform(transformation)
     o3d.visualization.draw_geometries([source_temp, target_temp])
 
-# read pcds
-pcd = readPCD(binPath)
-# o3d.visualization.draw_geometries([pcd])
-
-# get planes n's and c's
-worldPlane = generateWorldPlane(150, 150, 50)
-worldPlane.paint_uniform_color([1, 0.706, 0])
-
-# segment planes
-n_c, inliers = pcd.segment_plane(distance_threshold=0.25,
-                                         ransac_n=3,
-                                         num_iterations=1000)
-[a, b, c, d] = n_c
-n = [a, b, c]
-print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
-
-inlier_cloud = pcd.select_by_index(inliers)
-inlier_cloud.paint_uniform_color([1.0, 0, 0])
-outlier_cloud = pcd.select_by_index(inliers, invert=True).paint_uniform_color([0,0,1])
-
-
-# obtain plane parameterisations, n's and c's
-C_c = getC(n_c)
-circle_c = o3d.geometry.PointCloud()
-circle_c.points = o3d.utility.Vector3dVector(C_c)
-circle_c.paint_uniform_color([0, 1.0, 0])
-
-n_w = [0, 0, 1.0, 0]
-C_w = getC(n_w)
-circle_w = o3d.geometry.PointCloud()
-circle_w.points = o3d.utility.Vector3dVector(C_w)
-circle_w.paint_uniform_color([0, 0, 1.0])
-
-
-# o3d.visualization.draw_geometries([circle_c, inlier_cloud])
-# o3d.visualization.draw_geometries([circle_w, worldPlane])
-# o3d.visualization.draw_geometries([worldPlane, inlier_cloud])
-# o3d.visualization.draw_geometries([circle_c, circle_w])
-# o3d.visualization.draw_geometries([circle_c, worldPlane, inlier_cloud, circle_w])
-
-
-# coarse canonicalizaton
-alpha = np.arctan(-n_c[0]/n_c[2])                                               # roll
-beta = np.arctan(n_c[1]/(n_c[0] * np.cos(alpha)  -  n_c[0]*np.sin(alpha)))      # pitch
-
-# icp b/w circle_c and circle_w
-initRot = np.identity(4)
-R = o3d.geometry.get_rotation_matrix_from_zyx(np.array([beta, 0, alpha]))
-initRot[:3,:3] = R
-
-# draw_registration_result(pcd, worldPlane, np.identity(4))
-# draw_registration_result(pcd, worldPlane, initRot)
-
-# draw_registration_result(circle_c, circle_w, np.identity(4))
-# draw_registration_result(circle_c, circle_w, initRot)
-
-# for p in np.asarray(circle_c.points):
-#     print(p, np.linalg.norm(p))
-
-reg_p2p = o3d.pipelines.registration.registration_icp(
-    circle_c, circle_w, 2, initRot,
-    o3d.pipelines.registration.TransformationEstimationPointToPoint(),
-    o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=500))
-finalTransform = reg_p2p.transformation
-
-print("init rot: ", initRot)
-print("final rot: ", reg_p2p.transformation)
-
-print(reg_p2p)
-
-# draw_registration_result(circle_c, circle_w, finalTransform)
-
-# apply canonicalization
-canonicalPCD = pcd.transform(finalTransform)
-
-# draw_registration_result(pcd, worldPlane, np.identity(4))
-# draw_registration_result(canonicalPCD, worldPlane, np.identity(4))
-
 
 ## -- generate DEM --
 
@@ -214,9 +136,100 @@ def generateDEM(canonicalPCD, G_w, G_h, d):
 
     return DEM, subsampledPCD
 
+# read pcds
+pcd = readPCD(binPath)
+# o3d.visualization.draw_geometries([pcd])
+
+eul_ang = np.random.rand(3)*180/3.14
+eul_ang[2] =0 
+# print(eul_ang)
+
+print(o3d.geometry.get_rotation_matrix_from_xyz(eul_ang) )
+
+pcd.rotate(o3d.geometry.get_rotation_matrix_from_xyz(eul_ang))
+
+# get planes n's and c's
+worldPlane = generateWorldPlane(150, 150, 50)
+worldPlane.paint_uniform_color([1, 0.706, 0])
+
+# segment planes
+n_c, inliers = pcd.segment_plane(distance_threshold=0.25,
+                                         ransac_n=3,
+                                         num_iterations=1000)
+[a, b, c, d] = n_c
+n = [a, b, c]
+# print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
+
+inlier_cloud = pcd.select_by_index(inliers)
+inlier_cloud.paint_uniform_color([1.0, 0, 0])
+outlier_cloud = pcd.select_by_index(inliers, invert=True).paint_uniform_color([0,0,1])
+
+
+# obtain plane parameterisations, n's and c's
+C_c = getC(n_c)
+circle_c = o3d.geometry.PointCloud()
+circle_c.points = o3d.utility.Vector3dVector(C_c)
+circle_c.paint_uniform_color([0, 1.0, 0])
+
+n_w = [0, 0, 1.0, 0]
+C_w = getC(n_w)
+circle_w = o3d.geometry.PointCloud()
+circle_w.points = o3d.utility.Vector3dVector(C_w)
+circle_w.paint_uniform_color([0, 0, 1.0])
+
+
+# o3d.visualization.draw_geometries([circle_c, inlier_cloud])
+# o3d.visualization.draw_geometries([circle_w, worldPlane])
+# o3d.visualization.draw_geometries([worldPlane, inlier_cloud])
+# o3d.visualization.draw_geometries([circle_c, circle_w])
+# o3d.visualization.draw_geometries([circle_c, worldPlane, inlier_cloud, circle_w])
+
+
+# coarse canonicalizaton
+alpha = np.arctan(-n_c[0]/n_c[2])                                               # roll
+beta = np.arctan(n_c[1]/(n_c[0] * np.cos(alpha)  -  n_c[0]*np.sin(alpha)))      # pitch
+
+# icp b/w circle_c and circle_w
+initRot = np.identity(4)
+R = o3d.geometry.get_rotation_matrix_from_zyx(np.array([beta, 0, alpha]))
+initRot[:3,:3] = R
+
+print( initRot)
+
+# draw_registration_result(pcd, worldPlane, np.identity(4))
+# draw_registration_result(pcd, worldPlane, initRot)
+
+# draw_registration_result(circle_c, circle_w, np.identity(4))
+# draw_registration_result(circle_c, circle_w, initRot)
+
+# for p in np.asarray(circle_c.points):
+#     print(p, np.linalg.norm(p))
+
+reg_p2p = o3d.pipelines.registration.registration_icp(
+    circle_c, circle_w, 2, initRot,
+    o3d.pipelines.registration.TransformationEstimationPointToPoint(),
+    o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=500))
+finalTransform = reg_p2p.transformation
+
+# print("init rot: ", initRot)
+# print("final rot: ", reg_p2p.transformation)
+# print("diff: ", initRot @ reg_p2p.transformation)
+
+
+# print(reg_p2p)
+
+# draw_registration_result(circle_c, circle_w, finalTransform)
+
+# apply canonicalization
+
+# draw_registration_result(pcd, worldPlane, np.identity(4))
+# draw_registration_result(canonicalPCD, worldPlane, np.identity(4))
 
 mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
 mesh = mesh.scale(20, [0,0,0])
+
+o3d.visualization.draw_geometries([mesh, pcd])
+canonicalPCD = pcd.transform(finalTransform)
 o3d.visualization.draw_geometries([mesh, canonicalPCD])
 
 DEM, subPCD = generateDEM(canonicalPCD, 500, 500, 0.2)
@@ -225,9 +238,9 @@ o3d.visualization.draw_geometries([mesh, subPCD])
 DEM = cv2.resize(DEM, (800, 800), interpolation = cv2.INTER_AREA)
 # DEM = cv2.blur(DEM, (1,10))
 
-print(DEM)
-print(DEM.min())
-print(DEM.max())
+# print(DEM)
+# print(DEM.min())
+# print(DEM.max())
 
 DEM /= DEM.max()
 
